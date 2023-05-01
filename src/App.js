f50677ee5b79e220b79b157acf3e4c3b98fc4162
Naldo47 <reinaldo.santana444@gmail.com>
2023-04-30 20:46:55 -0400

    Page added

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+   <body>
+<header>
+    <h1>Reinaldo santana Cuello</h1>
+        <nav>
+     
+            <li><a href="https://www.google.com">About</a></li>
+            <li><a href="https://www.google.com">Work</a></li>
+            <li><a href="https://www.google.com">Contact</a></li>
+   
+    </nav>
+    </header>
+    <main>
+        <img src="./assets/img/artistic-alpha-coders-blue-robot-hd-wallpaper-preview.jpg" alt="placeholder image"/>
+        <h2>Projects Coming Soon!</h2>
+        <img src="./assets/img/programmers-programming-motivational-code-text-hd-wallpaper-thumb.jpg"/>
+        <h2>Projects Coming Soon!</h2>
+    </main>
+
+
+<section>
+    <p class="p">Hello, I'm a Software Developer student at Columbia, at the moment I don't have any project finished but in the very near future I will, stay tuned...</p>
+</section>
+<footer>
+    <label for="username">Name:</label>
+    <input id="username" type="text" placeholder="First Last" />
+    <label  for="email">E-mail:</label>
+    <input id="email" type="text" placeholder="me@example.com" />
+</footer>
+
+</body>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// TODO: Add a comment that describes the difference between react and react-dom
+// The react module is an entry point to the React library, while react-dom provides access to DOM-specific methods.
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
